eee76487267c959128c68ef37ceea02d95c338ac
Juan Diego Coto <jd506@hotmail.com>
2021-03-30 21:42:01 -0600

    Changing the background

--- src/views/Search/components/SearchResults/index.js
+++ src/views/Search/components/SearchResults/index.js
@@ -3,7 +3,7 @@ import SearchResultsItem from "./SearchResultsItem";
 export default function SearchResults({results, isSearching}) {
   return (
     <div style={{ width: "80%", padding: "0rem 2rem 0rem 2rem"}}>
-        {!results.length && isSearching && <p>Not exists!</p>}
+        {!results.length && isSearching && <p style={{color: "white"}}>Not exists!</p>}
         {/* <p>Not exists!</p> */}
         {results?.map((value) => 
             <SearchResultsItem key={value.id} {...value}/> 
